d700c28adf58d66991ee076d94b91d9a95eb1b84
Francisco Gleriston da Oliveira <88321125+FranckOliveira@users.noreply.github.com>
2022-03-04 19:49:37 -0300

    Add files via upload
    Solução para o exercício proposto.

---
+++ src/util/Calculator.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Calculator {
+	public static final double PI = 3.14159; //final é usado para atribuir constantes
+
+
+
+public static double circunferencia(double raio) {
+	return 2.0 * PI * raio;
+}
+
+public static double volume (double radio) {
+	return 4.0 * PI * radio*radio*radio /3.0;
+}
+}
